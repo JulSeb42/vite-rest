a74069fef07096e986d789539743f80fae32ed5f
JulSeb42 <julien.sebag@me.com>
2023-07-28 13:58:56 +0200

    Update eslintrc

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,16 +1,30 @@
 /* eslint-env node */
 module.exports = {
     root: true,
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    extends: ["plugin:react-hooks/recommended"],
     parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "eslint-plugin-import"],
+    plugins: [
+        "@typescript-eslint",
+        "eslint-plugin-import",
+        "eslint-plugin-react-hooks",
+        "react-hooks",
+    ],
     rules: {
         "import/no-duplicates": "warn",
         "@typescript-eslint/consistent-type-imports": "warn",
         "@typescript-eslint/no-unused-expressions": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                vars: "all",
+                args: "after-used",
+                ignoreRestSiblings: true,
+                destructuredArrayIgnorePattern: "[A-Z, _]",
+            },
+        ],
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+        "react-hooks/rules-of-hooks": "warn",
     },
 }
