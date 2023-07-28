8172a4cc70b7e8fb4e6b3629c4bcfd1715acddd4
JulSeb42 <julien.sebag@me.com>
2023-07-29 01:55:21 +0200

    Fix env client

--- client/src/api/http-common.ts
+++ client/src/api/http-common.ts
@@ -6,7 +6,7 @@
 
 import axios from "axios"
 
-const API_URI = import.meta.env.REACT_APP_API_URL || "http://localhost:5005"
+const API_URI = import.meta.env.VITE_API_URL || "http://localhost:5005"
 
 export const http = axios.create({
     baseURL: `${API_URI}/api`,
